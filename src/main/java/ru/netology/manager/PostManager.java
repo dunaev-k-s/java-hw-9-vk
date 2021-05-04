ce0d6d84070e1e257884cb2b921bb3bf4a924338
Konstantin Dunaev <shop.overled@gmail.com>
2021-05-04 21:25:30 +0300

    Task 2

---
+++ src/main/java/ru/netology/manager/PostManager.java
@@ -0,0 +1,14 @@
+package ru.netology.manager;
+
+import ru.netology.domain.Post;
+
+public class PostManager {
+    private Post[] posts;
+
+    public Post[] searchPost(int ownerId, String domain, String query, int ownersOnly, int count, int offset){
+        return null;
+    }
+
+    public void deletePost(int ownerId, int post_id){
+    }
+ }
